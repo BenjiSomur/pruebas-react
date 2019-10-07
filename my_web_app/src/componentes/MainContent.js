16bcb336808bc3467ff1562cedb65d80145ff19d
BenjiSomur <juan.somur@gmail.com>
2019-10-07 01:06:51 -0500

    Metiendo todo en un componente aparte unico

--- my_web_app/src/componentes/MainContent.js
+++ my_web_app/src/componentes/MainContent.js
@@ -1,21 +1,13 @@
 import React from 'react';
+import TodoComponent from './TodoComponent';
 
 function MainContent () {
 	return (
-		<main>
-			<div className="row">
-				<input type="checkbox" name="vehiculo1" value="Camioneta" />
-				<span>Camioneta</span>
-			</div>
-			<div className="row">
-				<input type="checkbox" name="vehiculo2" value="Sedan" />
-				<span>Sedan</span>
-			</div>
-			<div className="row">
-				<input type="checkbox" name="vehiculo3" value="Wagon" />
-				<span>Wagon</span>
-			</div>
-		</main>
+		<div className="todo-list">
+			<TodoComponent />
+			<TodoComponent />
+			<TodoComponent />
+		</div>
 	);
 }
 
---
+++ my_web_app/src/componentes/TodoComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoComponent(){
+	return (
+		<div className="todo-item">
+			<input type="checkbox" name="vehiculo1" value="Camioneta" />
+			<span>Camioneta</span>
+		</div>
+	);
+}
+
+export default TodoComponent;
