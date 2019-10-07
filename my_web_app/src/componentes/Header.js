fac65d8c2ac73d9dab94aae208a271668f7359f7
BenjiSomur <juan.somur@gmail.com>
2019-10-06 23:48:57 -0500

    Entendiendo javascript con jsx

--- my_web_app/src/componentes/Header.js
+++ my_web_app/src/componentes/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 function Header(){
+	const nombre = "Juan Carlos Benjamín";
+	const apellido = "Somohano Murrieta";
+
 	return (
 		<nav className="navbar">
-	        <h1>Hello World!</h1>
+	        <h1>Hello {`${nombre} ${apellido}`}!</h1>
 		</nav>
 	);
 }
