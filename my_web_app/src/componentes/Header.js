19ff99f362a97f474ec4ec786eb546b98d9ad570
BenjiSomur <juan.somur@gmail.com>
2019-10-06 23:22:20 -0500

    Separando componentes y agregandolos en App

---
+++ my_web_app/src/componentes/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header(){
+	return (
+		<nav>
+	        <h1>Hello World!</h1>
+	        <ul>
+	          <li>Cosa 1</li>
+	          <li>Cosa 2</li>
+	          <li>Cosa 3</li>
+	        </ul>
+		</nav>
+	);
+}
+
+export default Header;
---
+++ my_web_app/src/componentes/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent () {
+	return (
+		<main>
+			<p>Aqui va el resto del contenido...</p>
+		</main>
+	);
+}
+
+export default MainContent;
